3722065b166b51fe62a00187421e731faa265d00
faghihy <2982126+faghihy@users.noreply.github.com>
2024-06-19 15:20:00 -0400

    add tweet entity

---
+++ src/tweets/entities/tweet.entity.ts
@@ -0,0 +1,35 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  ManyToOne,
+  CreateDateColumn,
+} from 'typeorm';
+import { User } from 'src/users/entities/user.entity';
+
+@Entity()
+export class Tweet {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  content: string;
+
+  @ManyToOne(() => User)
+  author: User;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @Column({ nullable: true })
+  parentTweetId: string;
+
+  @Column({ type: 'simple-array', nullable: true })
+  hashtags: string[];
+
+  @Column({ nullable: true })
+  calegory: string;
+
+  @Column({ nullable: true })
+  location: string;
+}
