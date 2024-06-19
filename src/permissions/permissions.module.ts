c353cd1211d25d414b1089df30bb477b5369ce56
faghihy <2982126+faghihy@users.noreply.github.com>
2024-06-19 16:12:31 -0400

    add permissions service

---
+++ src/permissions/permissions.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Permission } from './entities/permission.entity';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([Permission])],
+  providers: [],
+  exports: [],
+})
+export class UsersModule {}
---
+++ src/permissions/permissions.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Permission } from './entities/permission.entity';
+
+@Injectable()
+export class PermissionsService {
+  constructor(
+    @InjectRepository(Permission)
+    private permissionsRepository: Repository<Permission>,
+  ) {}
+
+  getPermissions() {}
+  doPermissions() {}
+  canView() {}
+  canEdit() {}
+}
