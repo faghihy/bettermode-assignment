0b83a3a4032e96f92436bccddcf724c3dcee84c9
faghihy <2982126+faghihy@users.noreply.github.com>
2024-06-19 19:10:44 -0400

    impliment tweets resolver

--- src/tweets/tweets.resolver.ts
+++ src/tweets/tweets.resolver.ts
@@ -4,11 +4,28 @@ import { Tweet } from './entities/tweet.entity';
 
 @Resolver()
 export class TweetsResolver {
-  constructor(private tweetsService: TweetsResolver) {}
+  constructor(private tweetsService: TweetsService) {}
 
   @Query(() => [Tweet])
-  tweets() {}
+  tweets() {
+    return this.tweetsService.findAll();
+  }
 
   @Mutation(() => Tweet)
-  createTweet() {}
+  createTweet(
+    @Args('authorId') authorId: string,
+    @Args('content') content: string,
+    @Args('hashtags', { type: () => [String], nullable: true })
+    hashtags: string[],
+    @Args('category', { nullable: true }) category: string,
+    @Args('location', { nullable: true }) location: string,
+  ) {
+    return this.tweetsService.create(
+      authorId,
+      content,
+      hashtags,
+      category,
+      location,
+    );
+  }
 }
