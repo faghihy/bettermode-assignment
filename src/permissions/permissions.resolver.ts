e3654cd3b15668a4f2b7b4ae4e5d4157a001df18
faghihy <2982126+faghihy@users.noreply.github.com>
2024-06-19 21:14:04 -0400

    very first version of permission mechanism

--- src/permissions/permissions.resolver.ts
+++ src/permissions/permissions.resolver.ts
@@ -1,17 +1,38 @@
 import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
 import { PermissionsService } from './permissions.service';
 import { Permission } from './entities/permission.entity';
+import { Tweet } from '../tweets/entities/tweet.entity';
 
 @Resolver()
 export class PermissionsResolver {
   constructor(private permissionsService: PermissionsService) {}
 
   @Mutation(() => Permission)
-  updateTweetPermissions() {}
+  updateTweetPermissions(
+    @Args('tweetId') tweetId: Tweet,
+    @Args('inheritViewPermissions') inheritViewPermissions: boolean,
+    @Args('inheritEditPermissions') inheritEditPermissions: boolean,
+    @Args('viewPermissions', { type: () => [String], nullable: true })
+    viewPermissions: string[],
+    @Args('editPermissions', { type: () => [String], nullable: true })
+    editPermissions: string[],
+  ) {
+    return this.permissionsService.doPermissions(
+      tweetId,
+      inheritViewPermissions,
+      inheritEditPermissions,
+      viewPermissions,
+      editPermissions,
+    );
+  }
 
   @Query(() => Boolean)
-  canView() {}
+  canView(@Args('userId') userId: string, @Args('tweetId') tweetId: string) {
+    return this.permissionsService.canView(userId, tweetId);
+  }
 
   @Query(() => Boolean)
-  canEdit() {}
+  canEdit(@Args('userId') userId: string, @Args('tweetId') tweetId: string) {
+    return this.permissionsService.canEdit(userId, tweetId);
+  }
 }
